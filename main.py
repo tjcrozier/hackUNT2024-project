72f9198d56a9290634bb309c5a678abc69c357ae
Truitt Crozier <truittcrozier@gmail.com>
2024-10-26 17:46:17 -0500

    added ref to design.csss in sitface.html and edited main.py to include design.css as static file

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#url_for('static', filename='design.css')
+
 @app.route("/")
 def main():
     return render_template("siteface.html")
