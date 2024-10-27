8450271bef1bed351ca7b14a0a43d1e9c1e0e839
Truitt Crozier <truittcrozier@gmail.com>
2024-10-27 11:21:45 -0500

    added ai call to main.py  and edited test.html

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, Response, make_response
 from canopy import asin_to_json
+import ai_api_requests
 
 app = Flask(__name__)
 
@@ -11,7 +12,7 @@ def main():
 
 @app.post('/submit')
 def process_data():
-    return asin_to_json(request.get_data(as_text=True).split(","))
+    return ai_api_requests.getSuggestions(request.get_data(as_text=True).split(","))
     
 if __name__ == '__main__':
     app.run()
