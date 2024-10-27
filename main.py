5a203b0a18ae91ddfca687f5d5afd38658eff461
Truitt Crozier <truittcrozier@gmail.com>
2024-10-27 02:37:14 -0500

    setup test application that prints web input into console

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response, make_response
+import ai_api_requests
 
 app = Flask(__name__)
 
@@ -6,7 +7,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template("siteface.html")
+    return render_template("test.html")
 
+@app.post('/submit')
+def process_data():
+    print(request.get_data(as_text=True).split(","))
+    return make_response("foo")
+    
 if __name__ == '__main__':
     app.run()
